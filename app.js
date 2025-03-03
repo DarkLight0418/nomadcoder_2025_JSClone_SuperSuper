f51576e9cd5f1da5bbb732c5bcfc0a22d395481b
DarkLightBegin23 <samseong0418@naver.com>
2025-03-04 02:28:47 +0900

    20250304 js prac.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert("hi");
+console.log(4324324234);
+console.log("hihihi");
+console.log('가능');
+
+let a = 30;
+let b = 5;
+
+console.log(a + b);
+console.log(a / b);
+
+for (let i = 0; i < 10; i++) {
+    a += i;
+}
+
+console.log(a);
+
+const myNameWas = "messi ronaldo";
+
+console.log("good " + myNameWas);
