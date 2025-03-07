435b15f5db368b056d6c79fa67a3ec183381358a
DarkLightBegin23 <samseong0418@naver.com>
2025-03-07 09:01:18 +0900

    20250307 js array prac

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ console.log(something, amIFat);  // undifined
 let a = 30;
 let b = 5;
 const c = 3;
+
+const days = ["mon", "tue", "wed", "Thu", "Fri", "sat", "sun"];
+
+const noSense = [1, 2, "hello", false, null, true, undefined, "siuuuu"];
+
+console.log(noSense[2], days);
+
+noSense.push("oh");
+
+console.log(noSense)
