4094321f888d265968b2c1473762eabab6b8ef3f
DarkLightBegin23 <samseong0418@naver.com>
2025-04-12 09:47:17 +0900

    20250412 querySelector

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-document.title = "Hello From JS"  // HTML in Javascript
+const title = document.querySelectorAll(".dep h1");  // 모든 거 반환 <> querySelector 맨 앞의 꺼만 반환
+const ddd = document.getElementsByClassName("dep")
+
+console.log(title);
+console.log(ddd)
