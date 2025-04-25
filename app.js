e8fce12d79d0640a020f32b9ba4bbeb531b57e2c
DarkLightBegin23 <samseong0418@naver.com>
2025-04-25 15:07:56 +0900

    20250425 event listener

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const title = document.querySelector(".dep h1");  // querySelectorAll 모든 거 반환 <> querySelector 맨 앞의 꺼만 반환
+const h1 = document.querySelector(".dep h1");  // querySelectorAll 모든 거 반환 <> querySelector 맨 앞의 꺼만 반환
 // const ddd = document.getElementsByClassName("dep");
 
 // title.innerText = "Hello";  // innerText -> All 사용 시 작동 X (모든 요소에 적용)
@@ -6,19 +6,42 @@ const title = document.querySelector(".dep h1");  // querySelectorAll 모든 거
 
 function handleTitleClick() {
     console.log("title was clicked!");
-    title.style.color = "blue";  
+    h1.style.color = "blue";  
 }
 
 function handleMouseEnter() {
-    title.innerText = "Mouse is here!";
+    h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave() {
-    title.innerText = "Mouse is gone!";
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "skyblue"   
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function wifiOnline() {
+    alert("wifi connected!!")
+}
+
+function wifiOffline() {
+    alert("wifi disconnected")
 }
 
 // console.dir(title);  // event listen on~~: (ex)onmouseup)
 
-title.addEventListener("click", handleTitleClick);  // addEventListener(이벤트, 이벤트 발생 시 할 작업)
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+h1.addEventListener("click", handleTitleClick);  // addEventListener(이벤트, 이벤트 발생 시 할 작업)
+// title.onclick = handleTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+
+window.addEventListener("offline", wifiOffline);
+window.addEventListener("online", wifiOnline);
