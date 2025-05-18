ac55103ff1481d372cd16f5e20e1b71cd12d5b1e
DarkLightBegin23 <samseong0418@naver.com>
2025-05-18 18:57:02 +0900

    20250518 function argument and preventDefault

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // const loginForm = document.getElementsByClassName("login-form");  
 /* querySelector() => classname, tagname 모두 검색 가능 */
-const loginInput = document.querySelector(".login-form input");
-const loginButton = document.querySelector(".login-form button");
 
-function onLoginBtnClick() {
-    const userName = loginInput.value;
-    console.log(userName);
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
+
+function onLoginSubmit(event) {
+    event.preventDefault();  
+    /* preventDefault - 어떤 이벤트의 기본 동작 등이 발생되지 않도록 막는 메서드 */
+    // const userName = loginInput.value;
+    console.log(event);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);  // submit - Enter키 또는 버튼 클릭 시 발생
+
+/* func() - 함수 func을 '즉시' 실행 */
