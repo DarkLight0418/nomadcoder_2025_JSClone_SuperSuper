a797a76bba6eb458d09c4e63faabe3e62d9bb3c2
DarkLightBegin23 <samseong0418@naver.com>
2025-05-22 08:27:33 +0900

    20250522 addEventListener

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 
+// const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();  
     /* preventDefault - 어떤 이벤트의 기본 동작 등이 발생되지 않도록 막는 메서드 */
@@ -11,6 +13,14 @@ function onLoginSubmit(event) {
     console.log(event);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+    alert("clicked!!");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);  // submit - Enter키 또는 버튼 클릭 시 발생
+
+// link.addEventListener("click", handleLinkClick);  // 클릭 시 JS에서 실행시킴(addEventListener 안에 있는 함수는 직접 실행하지 않음)
 
 /* func() - 함수 func을 '즉시' 실행 */
