3b54a5d8ea3ca3cea89355c197e53fa4a76ed216
DarkLightBegin23 <samseong0418@naver.com>
2025-06-14 14:23:05 +0900

    20250614 quotes

--- JS/clock.js
+++ JS/clock.js
@@ -1,8 +1,8 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
+    const date = new Date();  // 시간 불러오기
+    const hours = String(date.getHours()).padStart(2, "0");  // String으로 감싸면 String으로 됨
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = (`${hours}:${minutes}:${seconds}`)
@@ -12,5 +12,6 @@ getClock();
 
 setInterval(getClock, 1000);
 
-// setInterval(함수, ms); interval - (매번의)
+// setInterval(함수, ms); interval - (매번의) 계속 시간을 업데이트 하고 싶어요 setInterval()
+// 1000ms == 1초
 
